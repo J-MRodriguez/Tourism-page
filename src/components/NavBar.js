20e9c25d1cc4cfd554f76491dc6e88ca8697bc30
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-08-09 22:37:57 -0300

    NavBar finished

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import '../components/NavBar.css';
 
 function NavBar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
     
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    };
+    
+    useEffect(() => {
+      showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
     return (
         <>
           <nav className="navbar">
               <div className="navbar-container">
-                <Link to="/" className="navbar-log">
-                  MagicTravel <i class="fab fa-maxcdn"></i>
+                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                    GTravel
+                    <i class="fas fa-globe-americas"></i>
                 </Link> 
                 <div className="menu-icon" onClick={handleClick}>
                   <i className={click ? "fas fa-times" : "fas fa-bars"}/>  
@@ -34,6 +53,7 @@ function NavBar() {
                         </Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
               </div>
           </nav>
         </>
