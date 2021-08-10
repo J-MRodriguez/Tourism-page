228883f71248d5c3343a5636d10a6053a30c9087
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-08-10 11:45:18 -0300

    finish GenSection

---
+++ src/components/GenSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../App.css';
+import './GenSection.css';
+import { Button } from './Button'
+
+function GenSection() {
+    return (
+        <div className='gen-container'>
+            <video src="assets/Splash - 23011.mp4"
+            autoPlay loop muted/>
+            <h1>ADVENTURE AWAITS</h1>
+            <p>what are you waiting for?</p>
+            <div className='gen-btns'>
+                <Button
+                 className='btns'
+                 buttonStyle='btn--outline'
+                 buttonSize='btn--large'
+                >
+                    GET STARTED
+                </Button>
+                <Button
+                 className='btns'
+                 buttonStyle='btn--primary'
+                 buttonSize='btn--large'
+                >
+                    WATH TRAILER <i className='far fa-play-circle'/>
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default GenSection
---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+import GenSection from '../GenSection.js';
+
+export default function Home(){
+    return(
+        <>
+         <GenSection/>
+        </>
+    );
+}
