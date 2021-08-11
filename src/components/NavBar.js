0b9b322588e4537f2b025c0f8f25b67eb6ab6313
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-08-11 10:51:40 -0300

    details

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
-import '../components/NavBar.css';
+import './Navbar.css';
 
 function NavBar() {
     const [click, setClick] = useState(false);
